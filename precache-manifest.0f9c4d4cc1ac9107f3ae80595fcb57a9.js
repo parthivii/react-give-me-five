e27f7d74a7ceaf1c8315d170877c4cf9e68e9039
parthivii <parthivisrivastava14@gmail.com>
2020-09-17 19:13:12 +0530

    Updates

---
+++ precache-manifest.0f9c4d4cc1ac9107f3ae80595fcb57a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ced6ff59bb3bd003700685808a31df5",
+    "url": "/react-give-me-five/index.html"
+  },
+  {
+    "revision": "0505fb17e2fb80ac19e8",
+    "url": "/react-give-me-five/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e28a2534ffa86313126a",
+    "url": "/react-give-me-five/static/css/main.0bae40d2.chunk.css"
+  },
+  {
+    "revision": "0505fb17e2fb80ac19e8",
+    "url": "/react-give-me-five/static/js/2.2d9c1276.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-give-me-five/static/js/2.2d9c1276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28a2534ffa86313126a",
+    "url": "/react-give-me-five/static/js/main.048aa598.chunk.js"
+  },
+  {
+    "revision": "435915a93efb269484ae",
+    "url": "/react-give-me-five/static/js/runtime-main.b72b8908.js"
+  }
+]);
